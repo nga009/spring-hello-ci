e62e3bdde5ce997125c15169ab0e645133915964
seno <norigm22@gmail.com>
2026-01-18 14:13:20 +0900

    修正

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void hello_ok() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello spring"));
+    }
+}
